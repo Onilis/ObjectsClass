89bd7bb56f6f126b0e657655de7000cf8230da0f
insai <insaittlg@gmail.com>
2025-01-27 09:14:47 +0300

    Начало работы Объекты и классы.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author author = new Author("Александр","Пушкин");
+        Book book = new Book("Капитанская дочка",author,1834);
+        book.setPublicationYear(1836);
+        System.out.println(book);
+
+        Author author1 = new Author("Лев","Толстой ");
+        Book book1 = new Book("Война и мир",author1,1873);
+        System.out.println(book1);
     }
 }
