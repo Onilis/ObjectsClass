4cd80765ec85c2065cd46817695d58aa790538c8
insai <insaittlg@gmail.com>
2025-01-27 09:15:55 +0300

    Готовая работа Объекты и классы.

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        Author author = new Author("Александр","Пушкин");
-        Book book = new Book("Капитанская дочка",author,1834);
+        Author author = new Author("Александр", "Пушкин");
+        Book book = new Book("Капитанская дочка", author, 1834);
         book.setPublicationYear(1836);
         System.out.println(book);
 
-        Author author1 = new Author("Лев","Толстой ");
-        Book book1 = new Book("Война и мир",author1,1873);
+        Author author1 = new Author("Лев", "Толстой ");
+        Book book1 = new Book("Война и мир", author1, 1873);
         System.out.println(book1);
     }
 }
